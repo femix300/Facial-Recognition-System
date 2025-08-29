1a2f5ce0f41ef55f9c000c84a684980adb4b1961
Qwertyemma <emmanuelobiasogu32@gmail.com>
2025-08-29 10:49:57 +0100

    added new files

--- core/settings.py
+++ core/settings.py
@@ -80,14 +80,20 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
+# using PostgreSQL for production
+DATABASES = {
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
